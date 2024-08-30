663063ec1510d572c564a038fbf4c08c27f7dd7f
superninjaX2 <lunguj665@gmail.com>
2024-08-30 10:42:04 +0200

    changed to rest api

--- frontend/src/js/handwriting/handwritting.js
+++ frontend/src/js/handwriting/handwritting.js
@@ -3,6 +3,7 @@ const editorElement = document.getElementById("editor");
 const editor = new iink.Editor(editorElement, {
 	configuration: {
 		server: {
+			protocal: "REST",
 			applicationKey: "9b3f6f32-9b70-4dbd-9e46-e4e7eda793c7",
 			hmacKey: "c69d1492-d59e-44b0-bfa9-a83fa5a3733c",
 		},
