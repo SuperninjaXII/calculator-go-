b4e6618de058aacc31913475757da06866567cb6
x2 <lunguj665@gmail.com>
2025-06-15 13:17:15 +0200

    race conditions

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,13 +1,23 @@
+import { Calculator } from "../wailsjs/go/main/App.js";
 
-import {Calculator} from "../wailsjs/go/main/App.js";
-
-let equal = document.querySelector("#equal")
-
-function test(){
-const result = Calculator("1 + 1")
-console.log(result.then((result)=>{
-  console.log(result)
-  }))
-}
-equal.addEventListener("click",test)
-
+const equal = document.querySelector("#equal");
+const inputValues = document.querySelector("#display-1");
+const Display = document.querySelector("#display-2");
+let answer = "";
+const Calculate = function (expression) {
+  const result = Calculator(expression);
+  result.then((result) => {
+    console.log(result);
+    answer = `${result}`;
+  });
+};
+const getValues = (data) => {
+  localStorage.setItem("input", `${data}`);
+  return localStorage.getItem("input");
+};
+equal.addEventListener("click", () => {
+  let data = getValues(inputValues.value);
+  Calculate(`${data}`);
+  console.log("this is answer" + answer);
+  Display.value = answer;
+});
