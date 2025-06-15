2d6c69fd14ab2ca3ebf47adbceaaf65fdae6899e
superninjaXII <lunguj665@gmail.com>
2025-06-15 21:12:11 +0200

    add button logic

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,23 +1,47 @@
 import { Calculator } from "../wailsjs/go/main/App.js";
 
 const equal = document.querySelector("#equal");
-const inputValues = document.querySelector("#display-1");
-const Display = document.querySelector("#display-2");
-let answer = "";
+const inputValues = document.querySelector("#display-top");
+const Display = document.querySelector("#display-bottom");
+//this function requiers and ecpressions as sn arguement and itut the result in the localstorage
 const Calculate = function (expression) {
   const result = Calculator(expression);
   result.then((result) => {
-    console.log(result);
-    answer = `${result}`;
+  localStorage.setItem("result", `${result}`);
   });
 };
-const getValues = (data) => {
-  localStorage.setItem("input", `${data}`);
-  return localStorage.getItem("input");
+//this is a helper function to get values from input
+const getValues = () => {
+  return localStorage.getItem("input")||"";
 };
-equal.addEventListener("click", () => {
-  let data = getValues(inputValues.value);
-  Calculate(`${data}`);
-  console.log("this is answer" + answer);
-  Display.value = answer;
+const setValues =()=>{
+  localStorage.setItem("input", `${inputValues.value}`);
+}
+
+const getButtonValues=()=>{
+  const buttons = document.querySelectorAll(`.numbers`);
+    setValues()
+
+  buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      const newValue = event.target.value;
+      const current = getValues();
+      let updated = current + newValue
+      localStorage.setItem("input", `${updated}`);
+    });
+  });
+}
+  //this displays to the output
+const DisplayAnswer=()=> {
+  let expression = getValues();
+
+if(expression.trim() != ""){
+  Calculate(`${expression}`);
+  localStorage.removeItem("input")
+  Display.innerHTML = answer;
+  }
 });
+
+equal.addEventListener("click", DisplayAnswer)
+//setup event listeners
+getButtonValues()
