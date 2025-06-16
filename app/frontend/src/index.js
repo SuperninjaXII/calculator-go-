73a685d799a7faad874be6908b6c182b56dabb83
x2 <lunguj665@gmail.com>
2025-06-16 22:07:14 +0200

    fixed merge conflict again

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -7,41 +7,41 @@ const Display = document.querySelector("#display-bottom");
 const Calculate = function (expression) {
   const result = Calculator(expression);
   result.then((result) => {
-  localStorage.setItem("result", `${result}`);
+    localStorage.setItem("result", `${result}`);
   });
 };
 //this is a helper function to get values from input
 const getValues = () => {
-  return localStorage.getItem("input")||"";
+  return localStorage.getItem("input") || "";
 };
-const setValues =()=>{
+const setValues = () => {
   localStorage.setItem("input", `${inputValues.value}`);
-}
+};
 
-const getButtonValues=()=>{
+const getButtonValues = () => {
   const buttons = document.querySelectorAll(`.numbers`);
-    setValues()
+  setValues();
 
-  buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
       const newValue = event.target.value;
       const current = getValues();
-      let updated = current + newValue
+      let updated = current + newValue;
       localStorage.setItem("input", `${updated}`);
     });
   });
-}
-  //this displays to the output
-const DisplayAnswer=()=> {
+};
+//this displays to the output
+const DisplayAnswer = () => {
   let expression = getValues();
 
-if(expression.trim() != ""){
-  Calculate(`${expression}`);
-  localStorage.removeItem("input")
-  Display.innerHTML = answer;
+  if (expression.trim() != "") {
+    Calculate(`${expression}`);
+    localStorage.removeItem("input");
+    Display.innerHTML = answer;
   }
-});
+};
 
-equal.addEventListener("click", DisplayAnswer)
+equal.addEventListener("click", DisplayAnswer);
 //setup event listeners//
-getButtonValues()
+getButtonValues();
